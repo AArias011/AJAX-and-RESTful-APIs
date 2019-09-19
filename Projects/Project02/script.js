520b93bb41db7b3774c7221dbedd3f30dc116f4a
Anthony Arias <aarias011@west-mec.org>
2019-09-18 18:24:39 -0700

    Added code:
    Had error that was hard to fix:

--- Projects/Project02/script.js
+++ Projects/Project02/script.js
@@ -1,10 +1,50 @@
 /*  Project 01_11_04
 
-    Author: 
+    Author: Anthony Arias
     Date:   
 
     Filename: script.js
 */
 
 "use strict";
+
+var httpRequest = false;
+
+function checkInput() {
+ var zip = document.getElementById("zip").nodeValue;
+ if (zip.length === 5) {
+     getLocation();
+ } else{
+     document.getElementById("city").value = "";
+     document.getElementById("state").value = "";
+ }
+}
+
+
+function getRequestObject() {
+    try {
+        httpRequest = new XMLHttpRequest();
+    } catch (requestError) {
+        return false;
+    }
+    console.log(httpRequest);
+    return httpRequest;
+}
+
+
+function getLocation() {
+    var zip = document.getElementById("zip").value;
+    if (!httpRequest) {
+        httpRequest = getRequestObject();
+    }
+}
+
+
+
+var zip = document.getElementById("zip");
+if (zip.addEventListener) {
+    zip.addEventListener("keyup", checkInput, false);
+} else if (zip.attachEvent) {
+    zip.attachEvent("onkeyup", checkInput);
+}
 
